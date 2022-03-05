e80ba60ae6be7e7b60589a8dcede1b877dca09be
Pipeloluwa Adebayo <pipeloluwa.adebayo@gmail.com>
2022-03-05 12:37:41 +0100

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -18,12 +17,12 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
       score++;
       document.querySelector(".score").textContent = score;
-      // if (score > highScore) {
-      //   highScore = score;
-      // } else {
-      //   highScore = highScore;
-      // }
-      // document.querySelector(".highscore").textContent = highScore;
+      if (score > highScore) {
+        highScore = score;
+      } else {
+        highScore = highScore;
+      }
+      document.querySelector(".highscore").textContent = highScore;
     }
   }
   if (guess !== secretNumber) {
@@ -43,33 +42,9 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").style.width = "30rem";
   }
 });
-  // if (guess > secretNumber) {
-  //   document.querySelector(".message").textContent = "❌ That was too high";
-  //   score--;
-  //   document.querySelector(".score").textContent = score;
-  //   document.querySelector("body").style.backgroundColor = "#222";
-  // } else if (guess < secretNumber) {
-  //   document.querySelector(".message").textContent = "❌ That was too low";
-  //   score--;
-  //   document.querySelector(".score").textContent = score;
-  // }
-  // } else {
-  //   document.querySelector(".message").textContent = "🔥 You lost the game";
-  //   score = 0;
-  //   document.querySelector(".score").textContent = score;
-  // }
-  // if (guess === secretNumber) {
-  //   document.querySelector("body").style.backgroundColor = "#60b347";
-  //   document.querySelector(".number").style.width = "30rem";
-  // } else {
-  // document.querySelector("body").style.backgroundColor = "#222";
-  // }
 
-document.querySelector('.highscore')=score>highScore?highScore=score:highScore=highScore
-
-
-document.querySelector(".again").addEventListener("click", function(){
-  score = 100;
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   document.querySelector(".message").textContent = "Start guessing...";
@@ -80,6 +55,27 @@ document.querySelector(".again").addEventListener("click", function(){
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
 });
+// if (guess > secretNumber) {
+//   document.querySelector(".message").textContent = "❌ That was too high";
+//   score--;
+//   document.querySelector(".score").textContent = score;
+//   document.querySelector("body").style.backgroundColor = "#222";
+// } else if (guess < secretNumber) {
+//   document.querySelector(".message").textContent = "❌ That was too low";
+//   score--;
+//   document.querySelector(".score").textContent = score;
+// }
+// } else {
+//   document.querySelector(".message").textContent = "🔥 You lost the game";
+//   score = 0;
+//   document.querySelector(".score").textContent = score;
+// }
+// if (guess === secretNumber) {
+//   document.querySelector("body").style.backgroundColor = "#60b347";
+//   document.querySelector(".number").style.width = "30rem";
+// } else {
+// document.querySelector("body").style.backgroundColor = "#222";
+// }
 
 // window.addEventListener("load", event => {
 //   document.querySelector(".again").onclick = function () {

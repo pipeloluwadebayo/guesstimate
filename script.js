7d10e6639b237883b4f828b18cec92c0a0acec3a
pipeloluwadebayo <pipeloluwapraise@gmail.com>
2022-06-22 14:26:49 +0100

    update

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let score = 20;
 let highScore = 0;
 
 //document.querySelector('.number').textContent = secretNumber;
-
-document.querySelector('.check').addEventListener('click', function () {
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    guesstimate();
+  }
+});
+const guesstimate = function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
@@ -14,6 +18,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎈 You guessed right!';
+    document.querySelector('.number').textContent = guess;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (score === 20) {
@@ -40,6 +45,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.message').textContent = '❌ That was too low';
   }
+};
+document.querySelector('.check').addEventListener('click', function () {
+  guesstimate();
 });
 
 document.querySelector('.again').addEventListener('click', function () {
